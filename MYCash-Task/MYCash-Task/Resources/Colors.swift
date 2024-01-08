7cd3052b06b393490328d6287fe874c6c531eb3f
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2024-01-08 22:20:10 +0200

    Add Icons And Colors 🎇🎨

---
+++ MYCash-Task/MYCash-Task/Resources/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  MYCash-Task
+//
+//  Created by iOSAYed on 08/01/2024.
+//
+
+import Foundation
+import UIKit
+
+internal enum Colors {
+  internal static let primaryTextColor = UIColor(named: "PrimaryTextColor")
+  internal static let secondTextColor = UIColor(named: "SecondTextColor")
+  internal static let PrimaryButtonColor = UIColor(named: "PrimaryButtonColor")
+    internal static let borderColor = UIColor(named: "BorderColor")
+    
+}
