d51112929c976e1f27d4e6263f459d88b238ba23
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2024-01-08 15:43:29 +0200

    create Splash Screen ViewController &  Login ViewController 🎬

---
+++ MYCash-Task/MYCash-Task/Modules/LoginVC/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  MYCash-Task
+//
+//  Created by iOSAYed on 08/01/2024.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    convenience required init() {
+        self.init(nibName: "\(LoginVC.self)", bundle: nil)
+    }
+
+}
