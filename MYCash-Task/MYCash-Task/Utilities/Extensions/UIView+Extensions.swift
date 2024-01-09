fe03a47b73ee2a8e609c313040cbb3b8a1a42ee3
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2024-01-10 01:57:34 +0200

    add new colors. 🎨

--- MYCash-Task/MYCash-Task/Utilities/Extensions/UIView+Extensions.swift
+++ MYCash-Task/MYCash-Task/Utilities/Extensions/UIView+Extensions.swift
@@ -8,6 +8,131 @@
 import UIKit
 extension UIView {
     
+    @IBInspectable
+        var cornerRadius: CGFloat {
+            get {
+                return layer.cornerRadius
+            }
+            set {
+                layer.cornerRadius = newValue
+            }
+        }
+        
+        @IBInspectable var topLeft: Bool {
+            get { return layer.maskedCorners.contains(.layerMinXMinYCorner) }
+            set {
+                if newValue {
+                    layer.maskedCorners.insert(.layerMinXMinYCorner)
+                } else {
+                    layer.maskedCorners.remove(.layerMinXMinYCorner)
+                }
+            }
+        }
+
+        @IBInspectable var topRight: Bool {
+            get { return layer.maskedCorners.contains(.layerMaxXMinYCorner) }
+            set {
+                if newValue {
+                    layer.maskedCorners.insert(.layerMaxXMinYCorner)
+                } else {
+                    layer.maskedCorners.remove(.layerMaxXMinYCorner)
+                }
+            }
+        }
+
+        @IBInspectable var bottomLeft: Bool {
+            get { return layer.maskedCorners.contains(.layerMinXMaxYCorner) }
+            set {
+                if newValue {
+                    layer.maskedCorners.insert(.layerMinXMaxYCorner)
+                } else {
+                    layer.maskedCorners.remove(.layerMinXMaxYCorner)
+                }
+            }
+        }
+
+        @IBInspectable var bottomRight: Bool {
+            get { return layer.maskedCorners.contains(.layerMaxXMaxYCorner) }
+            set {
+                if newValue {
+                    layer.maskedCorners.insert(.layerMaxXMaxYCorner)
+                } else {
+                    layer.maskedCorners.remove(.layerMaxXMaxYCorner)
+                }
+            }
+        }
+        
+        
+        
+        @IBInspectable
+        var borderWidth: CGFloat {
+            get {
+                return layer.borderWidth
+            }
+            set {
+                layer.borderWidth = newValue
+            }
+        }
+        @IBInspectable
+        var borderColor: UIColor? {
+            get {
+                if let color = layer.borderColor {
+                    return UIColor(cgColor: color)
+                }
+                return nil
+            }
+            set {
+                if let color = newValue {
+                    layer.borderColor = color.cgColor
+                } else {
+                    layer.borderColor = nil
+                }
+            }
+        }
+        @IBInspectable
+        var shadowRadius: CGFloat {
+            get {
+                return layer.shadowRadius
+            }
+            set {
+                layer.shadowRadius = newValue
+            }
+        }
+        @IBInspectable
+        var shadowOpacity: Float {
+            get {
+                return layer.shadowOpacity
+            }
+            set {
+                layer.shadowOpacity = newValue
+            }
+        }
+        @IBInspectable
+        var shadowOffset: CGSize {
+            get {
+                return layer.shadowOffset
+            }
+            set {
+                layer.shadowOffset = newValue
+            }
+        }
+        @IBInspectable
+        var shadowColor: UIColor? {
+            get {
+                if let color = layer.shadowColor {
+                    return UIColor(cgColor: color)
+                }
+                return nil
+            }
+            set {
+                if let color = newValue {
+                    layer.shadowColor = color.cgColor
+                } else {
+                    layer.shadowColor = nil
+                }
+            }
+        }
+    
     /// Loads a view from a nib file and adds it as a subview to the current view instance..
         func loadViewFromNib(bundle: Bundle? = nil) {
             let nibName = String(describing: Self.self)
@@ -55,6 +180,7 @@ extension UIView {
             layer.shadowOpacity = alpha
             layer.shadowOffset = CGSize(width: x, height: y)
             layer.shadowRadius = blur / 2.0
+            
             if spread == 0 {
                 layer.shadowPath = nil
             } else {
