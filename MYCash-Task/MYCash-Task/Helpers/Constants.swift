c1d4e6175db7f945a085ba28bc2a89ccc68907e4
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2024-01-09 21:17:49 +0200

    Create Constants File 💫

---
+++ MYCash-Task/MYCash-Task/Helpers/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  MYCash-Task
+//
+//  Created by iOSAYed on 09/01/2024.
+//
+
+import Foundation
+
+enum MyCashAPIURLs {
+    static let baseUrl = "https://yogahez.mountasher.online/api"
+}
+
+enum MYCashAPIPaths {
+    static let login = "/login"
+    static let clientProfile = "/client-profile"
+    
+}
