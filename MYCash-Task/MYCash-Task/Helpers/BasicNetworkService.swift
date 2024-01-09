d829d0b2726303d9160869c95c4fc8cdc4bc08da
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2024-01-09 21:07:11 +0200

    delete old logo icon 🗑️

---
+++ MYCash-Task/MYCash-Task/Helpers/BasicNetworkService.swift
@@ -0,0 +1,15 @@
+//
+//  sd.swift
+//  MYCash-Task
+//
+//  Created by iOSAYed on 09/01/2024.
+//
+
+import Foundation
+import RxSwift
+import Alamofire
+
+final class BasicNetworkService:NetworkService {
+    // 1 .JUST APPLY DEFAULT IMPLEMENATION
+
+}
